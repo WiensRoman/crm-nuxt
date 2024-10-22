64e374612e1744fffa771f44b329de1e797965af
Roman Wiens <wiensroman95@gmail.com>
2024-10-23 01:41:20 +0300

    подключил зависимости и добавил форму логина и сайдбар

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section>
+  <section class="grid" style="min-height: 100vh">
+    <LayoutSidebar />
     <div>
       <slot />
     </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/google-fonts",
+    "nuxt-icons",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+    "@nuxt/icon",
+  ],
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./components/ui",
+  },
+});
